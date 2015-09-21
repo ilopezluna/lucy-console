19baea326ea5b6e85192c88222777556a9b46d28
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-09-21 21:16:10 +0000

    dell customization

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var FS = require('fs');
 var App = require('express')();
 var ONE_HOUR = 1000 * 60 * 60;
 var ConsoleRouter = new (require('./router.js'))({
+  proxy: true,
   cache: process.env.DEVELOPMENT ? false : ONE_HOUR,
-  swagger: JSON.parse(FS.readFileSync(__dirname + '/examples/hacker_news.json')),
+  swagger: JSON.parse(FS.readFileSync(__dirname + '/examples/dell.json')),
 })
 
 App.use(ConsoleRouter.router);
